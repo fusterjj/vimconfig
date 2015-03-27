0c6739af58d9b43b9bd64461fa70610471f1715c
Joel Fuster <j@fuster.org>
2015-03-27 11:30:48 -0400

    Fix permissions and cleanup tags script

--- git-hooks/update_tags.sh
+++ git-hooks/update_tags.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ROOT=/c/Project_files/project
-INDEX_ROOT=/c/Project_files/project/.git
+INDEX_ROOT=$ROOT/.git
 PATHS="
    $ROOT/path1
    $ROOT/path2"
@@ -9,9 +9,9 @@ PATHS="
    echo
    for P in $PATHS
    do
-#TODO check for system type here
-      #echo find $P -iname "*.[chs]" -o -iname "*.[ppc]"
-      find $P -iname "*.[chs]" -o -iname "*.[ppc]" | cygpath -w -f -
+      #TODO check for system type here
+      find $P -iname "*.[chs]" -o -iname "*.[ppc]"
+      #find $P -iname "*.[chs]" -o -iname "*.[ppc]" | cygpath -w -f -
    done
 ) > $INDEX_ROOT/cscope.files
 
